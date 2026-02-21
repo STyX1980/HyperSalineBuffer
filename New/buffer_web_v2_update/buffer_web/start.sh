b095cebe302e4d4fb8894e4c8309e90ce5ec7232
Paz Nativ <paznativ@gmail.com>
2026-02-21 22:12:08 +0200

    Copy table button, centred regression stats

---
+++ New/buffer_web_v2_update/buffer_web/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+PORT="${PORT:-8080}"
+echo "Starting on port $PORT"
+exec gunicorn app:app --bind "0.0.0.0:$PORT" --workers 2 --timeout 120 --log-level info --access-logfile - --error-logfile -
